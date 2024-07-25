c73870455f57dda6e22d79669be37869ffb04dec
JoowonSuh <sjw2023@gmail.com>
2024-07-25 16:11:07 +0900

    add http server with routing library

--- my-blog/http-server.go
+++ my-blog/http-server.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+	MyRouter := mux.NewRouter()
+
+	MyRouter.HandleFunc("/{title}", func(resp http.ResponseWriter, req *http.Request) {
+		VarsInRequest := mux.Vars(req)
+		fmt.Println(VarsInRequest["title"])
+	})
+
+	MyRouter.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
 		fmt.Println("hello http server")
 	})
 
+	http.ListenAndServe(":8080", MyRouter)
+	// http.ListenAndServe(":8080", nil)
 }
